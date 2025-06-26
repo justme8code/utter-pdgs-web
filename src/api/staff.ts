26f233f1e973b64c0707983e5d306e668b6d9c19
justme8code <jvmMonster1@gmail.com>
2025-06-26 12:15:15 +0100

    updates to myrequest and makerequest in axios.ts and session.ts

--- src/api/staff.ts
+++ src/api/staff.ts
@@ -1,15 +1,10 @@
 'use server';
-import {verifySession} from "@/lib/session";
-import {myRequest} from "@/lib/axios";
 import {Staff} from "@/app/types";
+import {makeAuthRequest} from "@/lib/session";
 
 export async function fetchStaffs() {
-    const token = await verifySession();
-    return await myRequest<null, Staff[]>({
+    return await makeAuthRequest<null, Staff[]>({
         url: `/users/staffs`,
         method: "GET",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-        }
     });
 }
