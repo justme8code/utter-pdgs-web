9788d6b9c53e16040855726655f3996540d72f8f
justme8code <jvmMonster1@gmail.com>
2025-06-26 12:13:05 +0100

    updates to myrequest and makerequest in axios.ts and session.ts

--- src/api/production.ts
+++ src/api/production.ts
@@ -1,36 +1,23 @@
 'use server';
-import {myRequest} from "@/lib/axios";
-import {makeAuthRequest, verifySession} from "@/lib/session";
+import {makeAuthRequest} from "@/lib/session";
 import {Conversion, Production, ProductMix, Purchase} from "@/app/types";
 
 export async function fetchProductions(page: number, size: number) {
-    const token = await verifySession();
-    return await myRequest<null, Production[]>({
+    const {data} = await makeAuthRequest<null, Production[]>({
         url: `/productions?page=${page}&size=${size}`,
-        method: "GET",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-        }
+        method: "GET"
     });
+    return data??[]
 }
 
 
 export async function createProduction(production: { name: string, startDate: string, endDate: string }) {
-    const token = await verifySession();
-    const data = await myRequest<{ name: string, startDate: string, endDate: string }, Production>({
+    const {data,status,error} = await makeAuthRequest<{ name: string, startDate: string, endDate: string },Production>({
         url: `/productions`,
         method: "POST",
-        data: production,
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        }
-    });
-
-    return {
-        data: data.data,
-        status: data.status,
-        error: data.error,
-    }
+        data:production
+    })
+    return {data:data,status,error:error.message}
 }
 
 export async function fetchProduction(id: number) {
